05c8ea18524806b1e509c557d4e3ffb36dc40226
user <user@example.com>
2025-09-11 20:39:32 +0530

    feat: Reverted Netlify changes and added .env to .gitignore

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ console.log("Connecting to MONGO_URI:", process.env.MONGO_URI);
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(()=>{
     console.log('✅ MongoDB connected');
+    app.listen(PORT, ()=>console.log(`🚀 Server running on http://localhost:${PORT}`));
   })
   .catch(err=>{ console.error('MongoDB connection error:', err); process.exit(1);} );
-
-export default app;
